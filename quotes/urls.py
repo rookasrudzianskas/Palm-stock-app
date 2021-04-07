a201071c49406266f1a6ebb5cd135c6794475ee0
Rokas <rookas.rudzenskas@gmail.com>
2021-04-07 15:18:02 +0300

    Rokas:

--- quotes/urls.py
+++ quotes/urls.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
 from django.urls import path
-
+# from .views import home
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
 ]
