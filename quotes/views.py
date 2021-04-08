92d53ca13bb2a5ed63e040481ab0f312da71f534
Rokas <rookas.rudzenskas@gmail.com>
2021-04-08 10:14:23 +0300

    API

--- quotes/views.py
+++ quotes/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+
+    api_request = requests.get(
+        "https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_be5627477eda4219ab8663c382811c82")
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+    # pk_be5627477eda4219ab8663c382811c82
+
+    return render(request, 'home.html', {'api': api})
+
 
 def about(request):
     return render(request, 'about.html', {})
